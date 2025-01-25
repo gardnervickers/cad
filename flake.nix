f4cede46ba33c7ff66a30cc2cb26b0b06e53411b
Gardner Vickers <gardner@vickers.me>
2025-01-25 11:34:08 -0500

    WIP

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "Flake for my python cad project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+      ...
+
+    }:
+    let
+      inherit (nixpkgs) lib;
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.;};
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+      pyprojectOverrides = _final: _pref: {
+        
+      };
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      python = pkgs.python312;
+      pythonSet =
+        # Use base package set from pyproject.nix builders
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              overlay
+              pyprojectOverrides
+            ]
+          );
+    in
+    {
+      packages.aarch64-darwin.default = pythonSet.mkVirtualEnv "hello-world-env" workspace.deps.default;
+
+      devShells.aarch64-darwin = rec {
+        default = impure;
+        impure = pkgs.mkShell {
+          packages = [
+            python
+            pkgs.uv
+          ];
+          env =
+            {
+              # Prevent uv from managing Python downloads
+              UV_PYTHON_DOWNLOADS = "never";
+              # Force uv to use nixpkgs Python interpreter
+              UV_PYTHON = python.interpreter;
+            }
+            // lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
+          shellHook = ''
+            unset PYTHONPATH
+          '';
+      }; 
+      };
+    };
+}
---
+++ src/hello_world/__init__.py
@@ -0,0 +1,11 @@
+import build123d as bd
+
+
+def main():
+    print("Hello, I'm a module")
+
+
+line = bd.Line(((0, 0), (1, 1)))
+
+if __name__ == "__main__":
+    main()
